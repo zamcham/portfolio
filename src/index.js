a9c8ae30eedef298c352ed4f8caa8a2fd26cf2a6
zamcham <cristian@zamcham.com>
2023-04-17 00:44:18 -0400

    Initial setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import "./main.scss";
+
+// #region Number Animation
+// Get the span element
+var numberSpan = document.getElementById('y');
+
+// Define the target number (7 in this case)
+var targetNumber = 7;
+
+// Define the interval in milliseconds for the animation
+var interval = 150; // 100ms for example, you can adjust this to your preference
+
+// Define the current number (starting from 0)
+var currentNumber = 0;
+
+// Function to update the number in the span element
+function updateNumber() {
+  // Increment the current number
+  currentNumber++;
+  
+  // Update the content of the span element
+  numberSpan.textContent = currentNumber;
+  
+  // Check if the current number has reached the target number
+  if (currentNumber === targetNumber) {
+    clearInterval(numberInterval); // Stop the interval when target number is reached
+  }
+}
+
+// Start the interval to update the number
+var numberInterval = setInterval(updateNumber, interval);
+// #endregion
