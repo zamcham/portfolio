8a4accc9c8c2c13da67191182a74c8a5eca94f9f
zamcham <cristian@zamcham.com>
2023-04-25 18:56:34 -0400

    Add effects template

--- src/modals/bgParallax.js
+++ src/modals/bgParallax.js
@@ -3,6 +3,7 @@ const background2 = document.getElementById('bg2');
 const background3 = document.getElementById('bg3');
 const background4 = document.getElementById('bg4');
 const background5 = document.getElementById('bg5');
+const decoBox1 = document.getElementById('decobox');
 
 
 
@@ -16,12 +17,13 @@ export  const bgParallax = () => {
   background3.style.top = `${value * 0.6}px`;
   background4.style.top = `${value * 0.4}px`;
   background5.style.top = `${value * 0.5}px`;
-
+  decoBox1.style.top = `${(value/-20) * 2}px`;
   ticking = false;
 };
 
 window.addEventListener('scroll', () => {
   latestKnownScrollY = window.scrollY;
+  console.log(window.scrollY);
   if (!ticking) {
     window.requestAnimationFrame(bgParallax);
     ticking = true;
