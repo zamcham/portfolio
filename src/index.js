2274117917b808cb414bb2182033d7d628c2beb1
zamcham <cristian@zamcham.com>
2023-06-04 22:18:24 -0400

    add projects template

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./main.scss";
+import './modals/projectsData.js';
 import { updateNumber } from "./modals/numbersAnim";
 import { bgParallax } from "./modals/bgParallax";
 import startAnimations from "./modals/scrollAnimations";
